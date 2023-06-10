54357df8de11057602b1b32a760b539fa675506d
aperezNWO <alejandro.perez.acosta@hotmail.com>
2023-06-10 01:04:45 -0500

    Aplicativo [MCSD - ALGORITMO - DIJKSTRA] v[1.0.0.1] 1) Commit Inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, VERSION } from '@angular/core';
+//
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector    : 'app-root',
+  templateUrl : './app.component.html',
+  styleUrls   : ['./app.component.css']
 })
+//
 export class AppComponent {
-  title = 'AngularDijkstra';
-}
+  //
+  title             : string = "[MCSD - ALGORITMO - DIJKSTRA]"; 
+  appName           : string = "[MCSD - ALGORITMO - DIJKSTRA]";
+  appVersion        : string = '1.0.0.1';
+  runtimeVersion    : string = VERSION.full;
+ }   
