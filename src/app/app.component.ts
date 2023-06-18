e15c131a55fa199634dd3fe9ea98c72f04972517
alejandro.perez.acosta@hotmail.com <alejandro.perez.acosta@hotmail.com>
2023-06-18 00:50:53 -0500

    Aplicativo [MCSD - CONSULTAS] v[1.0.0.14] 1) CSS LINK CHANGES

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent {
     //
     title             : string = "[MCSD - CONSULTAS]"; 
     appName           : string = "[MCSD - CONSULTAS]";
-    appVersion        : string = '1.0.0.13';
+    appVersion        : string = '1.0.0.14';
     runtimeVersion    : string = VERSION.full;
     //
     readonly HomeWebComponent_pageTitle                   : string = HomeWebComponent.pageTitle();
