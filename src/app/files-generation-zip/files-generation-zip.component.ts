8adadda5bd604265a2e0669cf6bc6ea54d2edb48
alejandro.perez.acosta@hotmail.com <alejandro.perez.acosta@hotmail.com>
2023-06-29 18:51:21 -0500

    Aplicativo [MCSD - CONSULTAS] v[1.0.0.31] 1)  [GENERAR ARCHIVO ZIP] - Strategy.

--- src/app/files-generation-zip/files-generation-zip.component.ts
+++ src/app/files-generation-zip/files-generation-zip.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+//
 @Component({
   selector: 'app-files-generation-zip',
   templateUrl: './files-generation-zip.component.html',
   styleUrls: ['./files-generation-zip.component.css']
 })
+//
 export class FilesGenerationZIPComponent {
-
+    //--------------------------------------------------------------------------
+    // PROPIEDADES COMUNES
+    //--------------------------------------------------------------------------
+    pageTitle            : string = '[GENERAR ARCHIVO ZIP]';
+    //
+    static pageTitle()   : string {
+      return '[GENERAR ARCHIVOS ZIP]';
+    }
 }
