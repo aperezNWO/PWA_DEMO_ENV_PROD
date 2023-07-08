b17a75326def5e0849b54a3cce74c0080f8290d2
alejandro.perez.acosta@hotmail.com <alejandro.perez.acosta@hotmail.com>
2023-07-08 11:29:53 -0500

    Aplicativo [MCSD - CONSULTAS] v[1.0.0.40] 1) [ALGORITMOS - DISTANCIA MAS CORTA].

--- src/app/algorithm-dijkstra/algorithm-dijkstra.component.ts
+++ src/app/algorithm-dijkstra/algorithm-dijkstra.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+//
 @Component({
-  selector: 'app-algorithm-dijkstra',
-  templateUrl: './algorithm-dijkstra.component.html',
-  styleUrls: ['./algorithm-dijkstra.component.css']
+  selector       : 'app-algorithm-dijkstra',
+  templateUrl    : './algorithm-dijkstra.component.html',
+  styleUrls      : ['./algorithm-dijkstra.component.css']
 })
+//
 export class AlgorithmDijkstraComponent {
-
+  //--------------------------------------------------------------------------
+  // PROPIEDADES COMUNES
+  //--------------------------------------------------------------------------
+  pageTitle            : string = '[ALGORITMOS - DISTANCIA MAS CORTA]';
+  //
+  static pageTitle()   : string {
+    return '[ALGORITMOS - DISTANCIA MAS CORTA]';
+  }
 }
