f34160bbf4a045df0fa286ff8f60f1cb07c6adce
alejandro.perez.acosta@hotmail.com <alejandro.perez.acosta@hotmail.com>
2023-07-10 17:35:41 -0500

    Aplicativo [MCSD - CONSULTAS] v[1.0.0.46] 1) [ALGORITMOS - DISTANCIA MAS CORTA] - Drawing Shortest Path.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent {
     //
     title             : string = "[MCSD - CONSULTAS]"; 
     appName           : string = "[MCSD - CONSULTAS]";
-    appVersion        : string = '1.0.0.45';
+    appVersion        : string = '1.0.0.46';
     runtimeVersion    : string = VERSION.full;
     //
     readonly HomeWebComponent_pageTitle                   : string = HomeWebComponent.pageTitle();
