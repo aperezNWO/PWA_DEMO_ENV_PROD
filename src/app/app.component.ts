9deae1deff5c235f570ae7dc4f1155a3177a7952
alejandro.perez.acosta@hotmail.com <alejandro.perez.acosta@hotmail.com>
2023-08-09 15:57:52 -0500

    scm/ strategy update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,7 @@ export class AppComponent {
     //
     title             : string = "[MCSD - CONSULTAS]"; 
     appName           : string = "[MCSD - CONSULTAS]";
-    appVersion        : string = '1.0.0.73';
+    appVersion        : string = '1.0.0.74';
     runtimeVersion    : string = VERSION.full;
     //
     readonly HomeWebComponent_pageTitle                   : string  = HomeWebComponent.PageTitle;
