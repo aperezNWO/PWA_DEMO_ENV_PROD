2911b922b5c8b51d25637196095f38664dcf60d5
alejandro.perez.acosta@hotmail.com <aperezNWO>
2023-12-28 00:30:21 -0500

    Aplicativo [MCSD - CONSULTAS] v[1.0.1.13] 1) [GAMES - TIC-TAC-TOEI] a) OOP.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,7 +22,7 @@ export class AppComponent {
     public static title             : string | undefined = "[WEB API DEMO]"; 
     // propiedades internas
     public static appName           : string | undefined = "[WEB API DEMO]";
-    public static appVersion        : string | undefined = "[1.0.1.12]";
+    public static appVersion        : string | undefined = "[1.0.1.13]";
     // propiedades publicas
     public readonly _title                                       : string | undefined  = AppComponent.title;
     public readonly _appName                                     : string | undefined  = AppComponent.appName;
