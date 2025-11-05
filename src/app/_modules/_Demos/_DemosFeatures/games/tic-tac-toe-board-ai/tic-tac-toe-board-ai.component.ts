95badea565b5023f225ccf2b0733568a4536563c
aperezNWO <alejandro.perez.acosta@hotmail.com>
2025-11-04 21:25:43 -0500

    Application [ANGULAR DEMO - ENV_PROD] v[2.4.31]
    1) uix fix

--- src/app/_modules/_Demos/_DemosFeatures/games/tic-tac-toe-board-ai/tic-tac-toe-board-ai.component.ts
+++ src/app/_modules/_Demos/_DemosFeatures/games/tic-tac-toe-board-ai/tic-tac-toe-board-ai.component.ts
@@ -66,10 +66,11 @@ export class TicTacToeBoardAiComponent extends BaseComponent implements OnInit {
         // LENGUAJES DE PROGRAMACION
         //-----------------------------------------------------------------------------
         this.__languajeList = new Array();
-        this.__languajeList.push(new _languageName(0, '(.NET Core/C++ -> Expert       )'  , false ,"CPP"  ));
-        this.__languajeList.push(new _languageName(1, '(.NET Core/C++ -> Creative     )'  , true,  "CPP"   ));
-        this.__languajeList.push(new _languageName(3, '(.NET Core/C++ -> Random Player)'  , false, "CPP"  ));
-        this.__languajeList.push(new _languageName(4, '(Python        -> Tensorflow)   '  , false, "PY"   ));
+        //this.__languajeList.push(new _languageName(0, '(.NET Core/C++ -> Expert        ) '  , false ,"CPP"  ));
+        //this.__languajeList.push(new _languageName(1, '(.NET Core/C++ -> Creative      ) '  , true,  "CPP"  ));
+        this.__languajeList.push(new _languageName(2, '(.NET Core/C++ -> Min Max       ) '  , false, "CPP"  ));
+        this.__languajeList.push(new _languageName(3, '(.NET Core/C++ -> Random Player ) '  , true,  "CPP"  ));
+        this.__languajeList.push(new _languageName(4, '(Python        -> Tensorflow    ) '  , false, "PY"   ));
         //
         let langName = params['langName'] ? params['langName'] : "" ;
         //
@@ -92,7 +93,7 @@ export class TicTacToeBoardAiComponent extends BaseComponent implements OnInit {
         } 
         else 
         {
-            this.aiMode = 1;
+            this.aiMode = 3;
         }
     });
   }
