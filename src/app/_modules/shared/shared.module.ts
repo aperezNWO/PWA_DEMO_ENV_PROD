32b8e19e57c8677bd115846a88a4bd358ac1acda
aperezNWO <alejandro.perez.acosta@hotmail.com>
2026-02-14 00:38:45 -0500

    strategy update

--- src/app/_modules/shared/shared.module.ts
+++ src/app/_modules/shared/shared.module.ts
@@ -1,9 +1,8 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router'; // 1. Add this import
-
-import { BaseReferenceComponent } from '../../_components/base-reference/base-reference.component';
+import { NgModule                           } from '@angular/core';
+import { CommonModule                       } from '@angular/common';
+import { FormsModule, ReactiveFormsModule   } from '@angular/forms';
+import { RouterModule                       } from '@angular/router'; // 1. Add this import
+import { BaseReferenceComponent             } from '../../_components/base-reference/base-reference.component';
 
 @NgModule({
   declarations: [
