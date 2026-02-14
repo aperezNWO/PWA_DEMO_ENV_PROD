72196f352127357b79ad14fe322d549ce405c5f8
aperezNWO <alejandro.perez.acosta@hotmail.com>
2026-02-14 00:33:58 -0500

    strategy update

--- src/app/_modules/about/about.module.ts
+++ src/app/_modules/about/about.module.ts
@@ -3,16 +3,15 @@ import { NgModule                } from '@angular/core';
 import { CommonModule            } from '@angular/common';
 // CUSTOM MODULES
 import { SharedModule            } from '../shared/shared.module';
+import { BaseSortableHeader      } from 'src/app/_directives/sortable.directive';
 // CUSTOM COMPONENTS
 import { ContactformComponent    } from './contactform/contactform.component';
 import { IndexComponent          } from './index/index.component';
 import { SCMComponent            } from './scm/scm.component';
 import { TechnicalSpecsComponent } from './technicalspecs/technical-specs/technical-specs.component';
 import { SpeechPanelComponent    } from 'src/app/_components/speech-panel/speech-panel.component';
-
 // THIRD PARTY
 import { NgbHighlight, NgbPaginationModule, NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
-import { BaseSortableHeader } from 'src/app/_directives/sortable.directive';
 
 
 @NgModule({
